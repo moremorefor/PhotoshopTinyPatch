03ee74a19be8adc4e67d252f8be3e8e877aca4a6
more_more_for <more.more.for@gmail.com>
2012-12-06 23:59:10 +0900

    9patch tools

---
+++ 9patch-tools.jsx
@@ -0,0 +1,75 @@
+/* 
+<javascriptresource>
+<name>9-patch Tools</name>
+<category>mobile</category>
+</javascriptresource>
+*/
+
+/*
+ * auther: Tsukada Takumi (more_more_for)
+ * 
+ */
+// =======================================================
+var id1074 = charIDToTypeID( "setd" );
+    var desc184 = new ActionDescriptor();
+    var id1075 = charIDToTypeID( "null" );
+        var ref133 = new ActionReference();
+        var id1076 = charIDToTypeID( "Chnl" );
+        var id1077 = charIDToTypeID( "fsel" );
+        ref133.putProperty( id1076, id1077 );
+    desc184.putReference( id1075, ref133 );
+    var id1078 = charIDToTypeID( "T   " );
+        var desc185 = new ActionDescriptor();
+        var id1079 = charIDToTypeID( "Top " );
+        var id1080 = charIDToTypeID( "#Pxl" );
+        desc185.putUnitDouble( id1079, id1080, 0.000000 );
+        var id1081 = charIDToTypeID( "Left" );
+        var id1082 = charIDToTypeID( "#Pxl" );
+        desc185.putUnitDouble( id1081, id1082, 0.000000 );
+        var id1083 = charIDToTypeID( "Btom" );
+        var id1084 = charIDToTypeID( "#Pxl" );
+        desc185.putUnitDouble( id1083, id1084, 1.000000 );
+        var id1085 = charIDToTypeID( "Rght" );
+        var id1086 = charIDToTypeID( "#Pxl" );
+        desc185.putUnitDouble( id1085, id1086, 10.000000 );
+    var id1087 = charIDToTypeID( "Rctn" );
+    desc184.putObject( id1078, id1087, desc185 );
+executeAction( id1074, desc184, DialogModes.NO );
+
+// =======================================================
+var id1088 = charIDToTypeID( "Trnf" );
+    var desc186 = new ActionDescriptor();
+    var id1089 = charIDToTypeID( "null" );
+        var ref134 = new ActionReference();
+        var id1090 = charIDToTypeID( "Chnl" );
+        var id1091 = charIDToTypeID( "fsel" );
+        ref134.putProperty( id1090, id1091 );
+    desc186.putReference( id1089, ref134 );
+    var id1092 = charIDToTypeID( "FTcs" );
+    var id1093 = charIDToTypeID( "QCSt" );
+    var id1094 = charIDToTypeID( "Qcsa" );
+    desc186.putEnumerated( id1092, id1093, id1094 );
+    var id1095 = charIDToTypeID( "Ofst" );
+        var desc187 = new ActionDescriptor();
+        var id1096 = charIDToTypeID( "Hrzn" );
+        var id1097 = charIDToTypeID( "#Prc" );
+        desc187.putUnitDouble( id1096, id1097, 0 );
+        var id1098 = charIDToTypeID( "Vrtc" );
+        var id1099 = charIDToTypeID( "#Prc" );
+        desc187.putUnitDouble( id1098, id1099, 0 );
+    var id1100 = charIDToTypeID( "Ofst" );
+    desc186.putObject( id1095, id1100, desc187 );
+    var id1101 = charIDToTypeID( "Wdth" );
+    var id1102 = charIDToTypeID( "#Prc" );
+    desc186.putUnitDouble( id1101, id1102, 100.000000 );
+    var id1103 = charIDToTypeID( "Hght" );
+    var id1104 = charIDToTypeID( "#Prc" );
+    desc186.putUnitDouble( id1103, id1104, 100.000000 );
+executeAction( id1088, desc186, DialogModes.ALL );
+
+RGBColor = new SolidColor();
+RGBColor.red = 0;
+RGBColor.green = 0;
+RGBColor.blue = 0;
+
+activeDocument.selection.fill(RGBColor,ColorBlendMode.NORMAL, 100, false);
---
+++ 9patch.jsx
@@ -0,0 +1,103 @@
+/* 
+<javascriptresource>
+<name>9-patch</name>
+<category>mobile</category>
+</javascriptresource>
+*/
+
+/*
+ * auther: Tsukada Takumi (more_more_for)
+ * 
+ */
+
+preferences.rulerUnits = Units.PIXELS;
+
+var d = activeDocument;
+
+var docPath = d.fullName.toString(); //fullName:ドキュメントの保存されている絶対パス
+var docName = d.name.toString(); //ファイル名（拡張子含む）
+var filePath = docPath.split(docName)[0]; //Folderのパスを取得(カレントディレクトリ)
+var folderObj = new Folder(filePath); //Folderオブジェクトの作成
+var pFolderObj = new Folder(filePath+"9patch");
+
+//フォルダの検証・作成
+if (pFolderObj.exists) {
+} else {
+  var result = pFolderObj.create(); //戻り値はBoolean
+  if (result){
+  }else {
+    alert("フォルダが作成出来ませんでした");
+  };
+};
+
+var newDoc = d.duplicate();
+
+var w = newDoc.width;
+var h = newDoc.height;
+resizeOnePx(newDoc, w, h);
+
+//デフォルトの4点を作成
+makeDefaultDotSet(newDoc, w, h);
+
+saveObj();
+
+newDoc.close(SaveOptions.DONOTSAVECHANGES);
+d.close(SaveOptions.DONOTSAVECHANGES);
+
+/*
+ * 1px拡大
+ */
+function resizeOnePx(doc, w, h) {
+  //カンバスを全体で1px拡大
+  doc.resizeCanvas( w+2, h+2, AnchorPosition.MIDDLECENTER);
+}
+
+
+function saveObj() {
+  /*var webOpt = new ExportOptionsSaveForWeb(); // 保存Objectを作成
+  webOpt.format = SaveDocumentType.PNG;
+  webOpt.PNG8 = false; // PNG-24*/
+
+  var newFile = new File(pFolderObj.fsName+ "/" +docName.split(".")[0] + ".9.psd");
+  //newDoc.exportDocument(newFile, ExportType.SAVEFORWEB, webOpt);
+  newDoc.saveAs(newFile);
+}
+
+function makeDefaultDotSet(doc, docW, docH) {
+  makeDot(doc, "BOTTOM", [docW,docH+1], [docW+1,docH+1], [docW+1,docH+2], [docW,docH+2] );
+  makeDot(doc, "RIGHT", [docW+1,docH], [docW+2,docH], [docW+2,docH+1], [docW+1,docH+1] );
+  makeDot(doc, "LEFT", [0,1], [1,1], [1,2], [0,2] );
+  makeDot(doc, "UP", [1,0], [2,0], [2,1], [1,1] );
+}
+
+function makeDot(doc, layerName, select1, select2, select3, select4) {
+
+var layObj = doc.artLayers.add();
+doc.activeLayer = layObj;
+doc.activeLayer.name = layerName;
+
+var color = new SolidColor();
+color.red = 255;
+color.green = 0;
+color.blue = 0;
+color.model = ColorModel.RGB; 
+
+selReg = [select1, select2, select3, select4];
+activeDocument.selection.select(selReg);
+//activeDocument.selection.fill("black",ColorBlendMode.NORMAL, 100, false);
+var idFl = charIDToTypeID( "Fl  " );
+    var desc77 = new ActionDescriptor();
+    var idUsng = charIDToTypeID( "Usng" );
+    var idFlCn = charIDToTypeID( "FlCn" );
+    var idBlck = charIDToTypeID( "Blck" );
+    desc77.putEnumerated( idUsng, idFlCn, idBlck );
+    var idOpct = charIDToTypeID( "Opct" );
+    var idPrc = charIDToTypeID( "#Prc" );
+    desc77.putUnitDouble( idOpct, idPrc, 100.000000 );
+    var idMd = charIDToTypeID( "Md  " );
+    var idBlnM = charIDToTypeID( "BlnM" );
+    var idNrml = charIDToTypeID( "Nrml" );
+    desc77.putEnumerated( idMd, idBlnM, idNrml );
+executeAction( idFl, desc77, DialogModes.NO );
+
+}
