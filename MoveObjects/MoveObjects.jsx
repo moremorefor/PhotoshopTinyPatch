d93be27c1532f61deb2759eafd2066fdc777c8e3
more_more_for <more.more.for@gmail.com>
2013-05-15 14:22:13 +0900

    MoveObjects bug fix

--- MoveObjects/MoveObjects.jsx
+++ MoveObjects/MoveObjects.jsx
@@ -7,7 +7,7 @@
 
 /*
  * auther: Tsukada Takumi (@more_more_for)
- * ver: 1.0.1
+ * ver: 1.0.2
  */
 
 
@@ -32,15 +32,21 @@ function handle_key (event) {
 }
 
 function createDialog() {
+  //statictext
         var dlg = new Window('dialog', 'Move Objects', [100, 100, 250, 280]);
-        dlg.movingX = dlg.add('edittext', [10, 10, 60, 30]);
-        dlg.movingY = dlg.add('edittext', [90, 10, 140, 30]);
+        dlg.movingX = dlg.add('edittext', [40, 10, 140, 30]);
+        dlg.labelX = dlg.add('statictext',[10, 10, 30, 30] ,"X:",{multiline:true});
+        dlg.movingY = dlg.add('edittext', [40, 60, 140, 80]);
+        dlg.labelY = dlg.add('statictext',[10, 60, 30, 80], "Y:",{multiline:true});
 
         dlg.cancelButton = dlg.add('button', [10, 120, 100, 160], 'cancel');
         dlg.okButton = dlg.add('button', [110, 120, 140, 160], 'OK');
 
         dlg.movingX.addEventListener ("keydown", handle_key );
         dlg.movingY.addEventListener ("keydown",  handle_key );
+
+        dlg.movingX.text = 0;
+        dlg.movingY.text = 0;
         dlg.movingX.active = true;
         return dlg;
 }
@@ -53,7 +59,25 @@ function initializeDialog(w) {
   // }
   w.okButton.onClick = w.okButton.onClick = function() {
       var moveLayer = d.activeLayer;
-      moveLayer.translate(parseInt(w.movingX.text),parseInt(w.movingY.text));
+
+      var _x = w.movingX.text * 1.0;
+      var _y = w.movingY.text * 1.0;
+
+      var xx;
+      var yy;
+      if(_x == null){
+        xx = 0;
+      }else {
+        xx = _x;
+      }
+
+      if (_y == null) {
+        yy = 0;
+      }else {
+        yy = _y;
+      }
+
+      moveLayer.translate(xx,yy);
       w.close();
   }
 }
