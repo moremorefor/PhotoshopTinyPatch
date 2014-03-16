cd2494d7a27fb24c6eedbefe9d35bd1063111a8e
moremorefor <more.more.for@gmail.com>
2014-03-17 04:32:26 +0900

    Add duplicate option

--- MoveObjects/MoveObjects.jsx
+++ MoveObjects/MoveObjects.jsx
@@ -1,11 +1,13 @@
+/* 
+<javascriptresource>
+<name>Move Objects</name>
+<category>web</category>
+</javascriptresource>
+*/
+
 /*
- * MoveObjects.jsx
- *
- * Copyright (c) more_more_for.
- *
- * This software is released under the MIT License.
- * http://opensource.org/licenses/mit-license.php
- *
+ * auther: Tsukada Takumi (@more_more_for)
+ * ver: 1.0.3
  */
 
 
@@ -30,26 +32,80 @@ function handle_key (event) {
 }
 
 function createDialog() {
-  //statictext
-        var dlg = new Window('dialog', 'Move Objects', [100, 100, 250, 280]);
-        dlg.movingX = dlg.add('edittext', [40, 10, 140, 30]);
-        dlg.labelX = dlg.add('statictext',[10, 10, 30, 30] ,"X:",{multiline:true});
-        dlg.movingY = dlg.add('edittext', [40, 60, 140, 80]);
-        dlg.labelY = dlg.add('statictext',[10, 60, 30, 80], "Y:",{multiline:true});
 
-        dlg.cancelButton = dlg.add('button', [10, 120, 100, 160], 'cancel');
-        dlg.okButton = dlg.add('button', [110, 120, 140, 160], 'OK');
+  var wMargin = 20;
+  var hMargin = 10;
+  var wWidth = 340;
+  var wHeight = 240;
+  var characterHeight = 20;
+  var previousObj;
+  w = new Window("dialog","Move Objects",[100,100,100+wWidth,100+wHeight]);
 
-        dlg.movingX.addEventListener ("keydown", handle_key );
-        dlg.movingY.addEventListener ("keydown",  handle_key );
+  var textObjectX = new Object();
+  textObjectX.left = wMargin;
+  textObjectX.top = hMargin*2;
+  textObjectX.right = wWidth - 150 - wMargin;
+  textObjectX.bottom = textObjectX.top + characterHeight;
+  w.labelX = w.add("statictext", textObjectX, "moveX:");
 
-        dlg.movingX.text = 0;
-        dlg.movingY.text = 0;
-        dlg.movingX.active = true;
-        return dlg;
+  var editObjectX = new Object();
+  editObjectX.left = wMargin + 150;
+  editObjectX.top = hMargin*2;
+  editObjectX.right = wWidth - wMargin;
+  editObjectX.bottom = editObjectX.top + characterHeight;
+  w.movingX = w.add("edittext", editObjectX);
+  previousObj = editObjectX;
+
+  var textObjectY = new Object();
+  textObjectY.left = wMargin;
+  textObjectY.top = previousObj.bottom + hMargin*2;
+  textObjectY.right = wWidth - 150 - wMargin;
+  textObjectY.bottom = textObjectY.top + characterHeight;
+  w.labelY = w.add("statictext", textObjectY, "moveY:");
+
+  var editObjectY = new Object();
+  editObjectY.left = wMargin + 150;
+  editObjectY.top = previousObj.bottom + hMargin*2;
+  editObjectY.right = wWidth - wMargin;
+  editObjectY.bottom = editObjectY.top + characterHeight;
+  w.movingY = w.add("edittext", editObjectY);
+  previousObj = editObjectY;
+
+  var checkboxObj = new Object();
+  checkboxObj.left = wMargin;
+  checkboxObj.top = previousObj.bottom + hMargin*2;
+  checkboxObj.right = wWidth - wMargin;
+  checkboxObj.bottom = checkboxObj.top + characterHeight;
+  w.duplicate = w.add("checkbox", checkboxObj, "Duplicate");
+  previousObj = checkboxObj;
+
+  var cancelButtonObj = new Object();
+  cancelButtonObj.left = wMargin;
+  cancelButtonObj.top = previousObj.bottom + hMargin*2;
+  cancelButtonObj.right = wWidth - 160 - wMargin;
+  cancelButtonObj.bottom = cancelButtonObj.top + characterHeight*2;
+  w.cancelButton = w.add("button", cancelButtonObj, "cancel");
+
+  var okButtonObj = new Object();
+  okButtonObj.left = wMargin + 160;
+  okButtonObj.top = previousObj.bottom + hMargin*2;
+  okButtonObj.right = wWidth - wMargin;
+  okButtonObj.bottom = okButtonObj.top + characterHeight*2;
+  w.okButton = w.add("button", okButtonObj, "OK");
+  previousObj = okButtonObj;
+
+  w.movingX.addEventListener ("keydown", handle_key );
+  w.movingY.addEventListener ("keydown",  handle_key );
+
+  w.movingX.text = 0;
+  w.movingY.text = 0;
+  w.movingX.active = true;
+  w.duplicate.value = true;
+  return w;
 }
+
 function initializeDialog(w) {
-  w.okButton.onClick = w.okButton.onClick = function() {
+  w.okButton.onClick = function() {
       var moveLayer = d.activeLayer;
 
       var _x = w.movingX.text * 1.0;
@@ -69,7 +125,17 @@ function initializeDialog(w) {
         yy = _y;
       }
 
-      moveLayer.translate(xx,yy);
+      var duplicateOption = w.duplicate.value;
+
+      if(duplicateOption) {
+        _moveLayer = moveLayer;
+        var duplicateLayer = moveLayer.duplicate();
+        duplicateLayer.translate(xx,yy);
+
+      } else {
+        moveLayer.translate(xx,yy);
+      }
+
       w.close();
   }
 }
@@ -77,7 +143,7 @@ function initializeDialog(w) {
 function runDialog(w) {
     return w.show();
 };
-
+  
 function main() {
   var win = createDialog();
     initializeDialog(win);
